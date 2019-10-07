e402fb4e7feb31244be4de277baa43869b209466
amosmoyo <amosmoyo5300@gmail.com>
2019-10-08 00:33:03 +0300

    Made the blue Template of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { OrderItemsComponent } from './order-items/order-items.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavBarComponent,
+    ProductDetailsComponent,
+    ProductAlertsComponent,
+    OrderItemsComponent,
+    DeliveryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId',component:ProductDetailsComponent},
+      { path: 'cart',component:OrderItemsComponent},
+      { path:'shipping',component:DeliveryComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
