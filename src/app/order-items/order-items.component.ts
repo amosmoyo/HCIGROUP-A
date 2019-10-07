831fc137e4baae06f2251d97e3b8161e269eb32a
amosmoyo <amosmoyo5300@gmail.com>
2019-10-08 01:24:35 +0300

    change on the git order-items.component

--- src/app/order-items/order-items.component.ts
+++ src/app/order-items/order-items.component.ts
@@ -29,7 +29,7 @@ export class OrderItemsComponent implements OnInit {
 
   onSubmit(userdata:any){
 
-    this.cartService.clearCart();
+    this.items = this.cartService.clearCart();
     this.checkoutForm.reset();
   }
 
